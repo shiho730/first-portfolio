b4253133f05f9fb79b777f147449a3ba8282105f
EC2 Default User <ec2-user@ip-172-31-23-240.ap-northeast-1.compute.internal>
2020-12-01 04:06:56 +0000

    [fix]cart-layout

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,7 +1,8 @@
 class Like < ApplicationRecord
+
   belongs_to :customer
   belongs_to :item
-  
+
 #   validates_uniqueness_of :item_id, scope: :customer_id    # バリデーション（ユーザーと記事の組み合わせは一意）
 # # 同じ投稿を複数回お気に入り登録させないため。
 end
